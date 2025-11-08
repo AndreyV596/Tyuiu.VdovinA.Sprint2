1ca787c595b61eac37152d28bebd1553d7adfd24
AWUD <andrey.work596@gmail.com>
2025-11-09 02:55:16 +0500

    2

---
+++ Tyuiu.VdovinA.Sprint2.Task1.V14/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using Tyuiu.VdovinA.Sprint2.Task1.V14.Lib;
+
+namespace Tyuiu.VdovinA.Sprint2.Task1.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int a = 185;
+            int b = 216;
+            int c = 174;
+            int d = 917;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            Console.Title = "Спринт #2 |Выполнил: Вдовин А.С. | Пктб-25";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Вдовин Андрей Сергеевич | Пктб-25                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("* последовательность можно чередовать, но использовать один раз в         *");
+            Console.WriteLine("* выражении) и логических операций (|, &, ||, &&, !, ^, последовательность*");
+            Console.WriteLine("* операций не должна нарушаться), а также арифметических выражений,       *");
+            Console.WriteLine("* которая вернет логическую последовательность(массив)                    *");
+            Console.WriteLine("* (False, True, True, True, True, False) при a = 185, b = 216,           *");
+            Console.WriteLine("* c = 174, d = 917                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("c = " + c);
+            Console.WriteLine("d = " + d);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
