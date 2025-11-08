af720c329432ed44b8cace49c451be573eca825a
AWUD <andrey.work596@gmail.com>
2025-11-09 02:56:35 +0500

    уцуац

--- Tyuiu.VdovinA.Sprint2.Task1.V14.Lib/DataService.cs
+++ Tyuiu.VdovinA.Sprint2.Task1.V14.Lib/DataService.cs
@@ -6,12 +6,13 @@ namespace Tyuiu.VdovinA.Sprint2.Task1.V14.Lib
         public bool[] GetLogicOperations(int a, int b, int c, int d)
         {
             bool[] res = new bool[6];
-            res[0] = (a == b) & (c < d);
-            res[1] = (a != b) | (c > d);
-            res[2] = (a < b) || (c == d);
-            res[3] = (a > b) && (c <= d) ^ (a != c);
-            res[4] = !(a >= b) | (c != d) && (b > c);
-            res[5] = (a <= b) & (c >= d);
+
+            res[0] = (a > b) | (c == d);
+            res[1] = (a != b) & (c < d);
+            res[2] = (a <= b) || (c > d);
+            res[3] = !(a == c) && (b >= d) ^ (a < b);
+            res[4] = (a < b) | (c != d) && !(b <= c);
+            res[5] = (a >= b) & (c <= d);
 
             return res;
         }
