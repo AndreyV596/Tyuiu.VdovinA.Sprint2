e724bc0ea9073afc683a46277ea0e33e901b8ad7
AWUD <andrey.work596@gmail.com>
2025-11-09 02:23:04 +0500

    eewefwew

--- Tyuiu.VdovinA.Sprint2.Task0.V27.Lib/DataService.cs
+++ Tyuiu.VdovinA.Sprint2.Task0.V27.Lib/DataService.cs
@@ -12,6 +12,7 @@ namespace Tyuiu.VdovinA.Sprint2.Task0.V27.Lib
             res[0] = (x - 1030) == y;
             res[1] = (x - 1030) != y;
             res[2] = (x - 1031) < y;
+
             res[3] = (x - 1031) > y;
             res[4] = (x - 1030) <= y;
             res[5] = (x - 1031) >= y;
