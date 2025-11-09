95eaa2378bebca15a98528aabeb4a4d08304163b
AWUD <andrey.work596@gmail.com>
2025-11-09 16:19:04 +0500

    уцпцупцупцуп

---
+++ Tyuiu.VdovinA.Sprint2.Task4.V14.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.VdovinA.Sprint2.Task4.V14.Lib;
+namespace Tyuiu.VdovinA.Sprint2.Task4.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalculateFirstCondition()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 10;
+            double res = ds.Calculate(x, y);
+            double wait = 380.427; // (2 + 1/4)^10 = (2.25)^10 ≈ 380.427
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void CheckCalculateSecondCondition()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 5;
+            double res = ds.Calculate(x, y);
+            double wait = 0.998; // (25 - cos²(10) + 10)/(100 - sin²(5) + 12) ≈ 0.998
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VdovinA.Sprint2.Task4.V14/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.VdovinA.Sprint2.Task4.V14.Lib;
+namespace Tyuiu.VdovinA.Sprint2.Task4.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 |Выполнил: Вдовин Андрей | ПКТБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*  Спринт #2                                                              *");
+            Console.WriteLine("*  Тема: Тернарный оператор                                               *");
+            Console.WriteLine("*  Задание #04                                                            *");
+            Console.WriteLine("*  Вариант #14                                                            *");
+            Console.WriteLine("*  Выполнил: Вдовин Андрей | ПКТБ-25-1                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*  УСЛОВИЕ:                                                               *");
+            Console.WriteLine("*  Написать программу, которая вычисляет требуемое значение с             *");
+            Console.WriteLine("*  использованием тернарного оператора, где пользователь вводит значение  *");
+            Console.WriteLine("*  переменных x, y с клавиатуры                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*  ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+
+            Console.WriteLine("Введите значение переменной X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной Y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double res = ds.Calculate(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*  РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Значение функции = " + res);
+            Console.ReadKey();
+        }
+    }
+}
